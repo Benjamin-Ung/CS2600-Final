525ca5a2a498ec0bf517bc8c69e2b194fb371f43
Ben <benjaminung6035@gmail.com>
2021-12-10 05:45:46 +0000

    step 3 cont: fixed wasd to have boundaries

--- kilo.c
+++ kilo.c
@@ -13,6 +13,12 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+   ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
 
 /*** data ***/
 struct editorConfig {
@@ -53,7 +59,7 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -65,10 +71,10 @@ char editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if (seq[0] == '[') {
       switch (seq[1]) {
-        case 'A': return 'w';
-        case 'B': return 's';
-        case 'C': return 'd';
-        case 'D': return 'a';
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
       }
     }
     return '\x1b';
@@ -169,34 +175,43 @@ void editorRefreshScreen() {
 
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
-    case 'a':
-      E.cx--;
-      break;
-    case 'd':
-      E.cx++;
-      break;
-    case 'w':
-      E.cy--;
-      break;
-    case 's':
-      E.cy++;
-      break;
+    case ARROW_LEFT:
+        if (E.cx != 0) {
+            E.cx--;
+            break;
+        }
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1) {
+            E.cx++;
+            break;
+         }
+    case ARROW_UP:
+        if (E.cy != 0) {
+            E.cy--;
+            break;
+        }
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1) {
+            E.cy++;
+            break;
+        }
   }
 }
+
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c) {
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
-        case 'w':
-        case 's':
-        case 'a':
-        case 'd':
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
     }
